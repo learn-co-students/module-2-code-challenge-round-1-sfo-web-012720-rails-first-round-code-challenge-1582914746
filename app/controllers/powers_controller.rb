b72c2755f15885ecf77576124f9e42f7a19b6a88
Sylvia <sylviapap1@gmail.com>
2020-02-28 12:08:03 -0800

    mod2 cc

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,29 @@
 class PowersController < ApplicationController
+
+  before_action :find_power, only: [:show, :edit, :update]
+
   def index
     @powers = Power.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @power.update(power_params)
+    redirect_to powers_path
+  end
+
+  private
+
+  def power_params
+    params.require(:power).permit(:name, :description, heroine_ids: [])
+  end
+
+  def find_power
+    @power = Power.find(params[:id])
   end
 end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,9 @@
 class Heroine < ApplicationRecord
+    has_many :heroine_powers
+    has_many :powers, through: :heroine_powers
+    # accepts_nested_attributes_for :powers
+
+    validates :name, presence: true
+    validates :super_name, presence: true
+    validates :super_name, uniqueness: true
 end
