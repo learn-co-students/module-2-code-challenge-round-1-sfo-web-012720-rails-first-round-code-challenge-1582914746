97022bd28eac3f71ddf96dba574bfb9547ded739
austinoso <osorio_austin@yahoo.com>
2020-02-28 12:08:05 -0800

    Submiting code challenge

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,31 @@
 class HeroinesController < ApplicationController
+
   def index
     @heroines = Heroine.all
+  end
+
+  def show
+    @heroine = Heroine.find params[:id]
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.create(heroine_params.except(:powers))
+    @power = Power.find heroine_params[:powers]
+    if @heroine.valid?
+      @heroine.powers << @power
+      redirect_to @heroine
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit!
   end
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,28 @@
 class PowersController < ApplicationController
+  before_action :set_power, only: [:show, :edit, :update]
+  
   def index
     @powers = Power.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @power.update(power_params)
+    redirect_to @power
+  end
+
+  private
+  
+  def set_power
+    @power = Power.find params[:id]
+  end
+
+  def power_params
+    params.require(:power).permit!
   end
 end
